2ad7f50319a57185973fd7e027193914b35f4f42
Shaun Scaling <shaun@shaunscaling.com>
2018-02-16 22:21:52 +0000

    Hack in error handling

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"log"
 
@@ -27,17 +28,36 @@ func main() {
 
 	buff := bytes.NewBuffer(data)
 
+	for err := readMessage(buff); err != nil; {
+
+	}
+
+	log.Println("Done")
+}
+
+func readMessage(buff *bytes.Buffer) error {
 	// Get the field number
-	field := readField(buff)
+	field, err := readField(buff)
+	if err != nil {
+		return err
+	}
+
 	log.Printf("%#v\n", field)
 
 	switch field.WireType {
 	case 0: // varint
-		log.Printf("%d\n", readVarInt(buff))
+		i, err := readVarInt(buff)
+		if err != nil {
+			return err
+		}
+		log.Printf("%d\n", i)
 
 	default:
 		log.Fatalln("unrecognized WireType")
+		return errors.New("unrecognised WireType")
 	}
+
+	return nil
 }
 
 type Field struct {
@@ -45,11 +65,14 @@ type Field struct {
 	WireType int32
 }
 
-func readField(buff *bytes.Buffer) Field {
-	f := &Field{}
+func readField(buff *bytes.Buffer) (Field, error) {
+	f := Field{}
 
-	b, _ := buff.ReadByte()
-	// FIXME: Handle error
+	b, err := buff.ReadByte()
+	if err != nil {
+		return f, err
+	}
+
 	f.WireType = int32(b & 0x7)
 
 	// field Id is a varint
@@ -57,27 +80,38 @@ func readField(buff *bytes.Buffer) Field {
 
 	if (b & 0x80) != 0 {
 		log.Println("has more data")
-		log.Fatalln("Not implemented")
+		// FIXME: is this correct?
+		v, err := readVarInt(buff)
+		if err != nil {
+			return f, err
+		}
+		f.Id |= int32(v << 4)
 	} else {
 		log.Println("no more data")
 	}
 
-	return *f
+	return f, nil
 }
 
-func readVarInt(buff *bytes.Buffer) int64 {
+func readVarInt(buff *bytes.Buffer) (int64, error) {
 
-	b, _ := buff.ReadByte()
+	b, err := buff.ReadByte()
+	if err != nil {
+		return int64(0), err
+	}
 
 	v := int64(b & 0x7F)
 
 	chunks := 1
 	for (b & 0x80) != 0 {
-		b, _ = buff.ReadByte()
+		b, err = buff.ReadByte()
+		if err != nil {
+			return int64(0), err
+		}
 
 		v |= int64((b & 0x7F) << uint(7*chunks))
 		chunks++
 	}
 
-	return v
+	return v, nil
 }
